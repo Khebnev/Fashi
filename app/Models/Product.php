5722e01710f7bb4e7bf8daf71c9226d00c7b5968
Anton <khebnev.a.v@gmail.com>
2022-09-05 17:00:49 +0300

    Added relation in Category hasMany Product, Tag belongsToMany Product, Product where tags belongsToManyTags and category belongsTo Category

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tags::class);
+    }
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
