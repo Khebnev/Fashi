b2a4b6deaf935a10ce60e641715333f230d4498b
Anton <khebnev.a.v@gmail.com>
2022-09-09 23:10:48 +0300

    Deleted all categories. Added 3 from html-pattern. Increased pagination to 3

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -15,7 +15,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        $categories = Category::paginate(2);
+        $categories = Category::paginate(3);
 
         return view('admin.categories.index', compact('categories'));
     }
