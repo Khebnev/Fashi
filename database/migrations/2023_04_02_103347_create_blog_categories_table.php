c2f9aae6175cb2bd11e3ecd538b614ac31555b15
Anton <khebnev.a.v@gmail.com>
2023-04-02 14:04:09 +0300

    Added model BlogCategory. Added blog_categories_table and modified create_posts_table

--- database/migrations/2023_04_02_103347_create_blog_categories_table.php
+++ database/migrations/2023_04_02_103347_create_blog_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class CreateBlogCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,10 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('blog_categories', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
             $table->string('slug');
-            $table->text('description');
-            $table->text('content');
-            $table->integer('category_id')->unsigned();
-            $table->integer('view')->unsigned()->default(0);
-            $table->string('thumbnail')->nulluble();
             $table->timestamps();
         });
     }
@@ -33,6 +28,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('blog_categories');
     }
 }
