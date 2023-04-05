10c9f5eecb4c49fbba82c41b78b36a406c55300c
Anton <khebnev.a.v@gmail.com>
2023-04-05 15:24:34 +0300

    Added sluggable into Post and BlogCategory models. Added relatives between blogcategory and post

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class BlogCategory extends Model
 {
     use HasFactory;
+    use Sluggable;
+
+    protected $fillable = ['title'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,31 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     use HasFactory;
+    use Sluggable;
+
+    public function category()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+     public function sluggable(): array
+    {
+        return [
+        'slug' => [
+        'source' => 'title'
+    ]
+    ];
+    }
 }
